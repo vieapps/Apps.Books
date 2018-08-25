f1ed2379d0949e511f0e772ee4b157c291e2dee9
Quynh Nguyen <mr.quynh@gmail.com>
2018-08-26 06:41:36 +0700

    Refactoring: instances

--- src/app/components/app.pagination.ts
+++ src/app/components/app.pagination.ts
@@ -1,9 +1,11 @@
-import { AppData } from "../app.data";
+import * as Collections from "typescript-collections";
 import { AppCrypto } from "./app.crypto";
 import { AppUtility } from "./app.utility";
 
 /** Servicing component for working with paginations */
 export class AppPagination {
+	/** All pagination instances */
+	public static instances = new Collections.Dictionary<string, { TotalRecords: number, TotalPages: number, PageSize: number, PageNumber: number }>();
 
 	private static getFilterBy(info?: any) {
 		return AppUtility.isObject(info, true) && AppUtility.isObject(info.FilterBy, true)
@@ -57,10 +59,10 @@ export class AppPagination {
 	}
 
 	/** Gets a pagination */
-	public static get(info?: any, prefix?: string): { TotalRecords: number, TotalPages: number, PageSize: number, PageNumber: number } {
+	public static get(info?: any, prefix?: string) {
 		const id = this.getKey(info, prefix);
 		return AppUtility.isNotEmpty(id)
-			? AppData.paginations[id]
+			? this.instances.getValue(id)
 			: undefined;
 	}
 
@@ -68,7 +70,7 @@ export class AppPagination {
 	public static set(info?: any, prefix?: string) {
 		const id = this.getKey(info, prefix);
 		if (AppUtility.isNotEmpty(id)) {
-			AppData.paginations[id] = this.getDefault(info);
+			this.instances.setValue(id, this.getDefault(info));
 		}
 	}
 
@@ -76,7 +78,7 @@ export class AppPagination {
 	public static remove(info?: any, prefix?: string) {
 		const id = this.getKey(info, prefix);
 		if (AppUtility.isNotEmpty(id)) {
-			delete AppData.paginations[id];
+			this.instances.remove(id);
 		}
 	}
 
@@ -90,7 +92,7 @@ export class AppPagination {
 	}
 
 	/** Builds the well-formed request (contains filter, sort and pagination) for working with remote APIs */
-	public static buildRequest(filterBy?: any, sortBy?: any, pagination?: any, onPreCompleted?: (req: { FilterBy: any, SortBy: any, Pagination: any }) => void): { FilterBy: any, SortBy: any, Pagination: any } {
+	public static buildRequest(filterBy?: any, sortBy?: any, pagination?: any, onPreCompleted?: (request: { FilterBy: any, SortBy: any, Pagination: any }) => void): { FilterBy: any, SortBy: any, Pagination: any } {
 		const request = {
 			FilterBy: AppUtility.isObject(filterBy, true) ? AppUtility.clone(filterBy) : {},
 			SortBy: AppUtility.isObject(sortBy, true) ? AppUtility.clone(sortBy) : {},
