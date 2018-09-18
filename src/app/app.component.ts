61f0a65f4ddeaed2cf722c2732437dbb02c5e4dd
Quynh Nguyen <mr.quynh@gmail.com>
2018-09-18 21:32:03 +0700

    Improvement: upgrade to latest components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -314,38 +314,30 @@ export class AppComponent implements OnInit {
 			await this.usersSvc.activateAsync(
 				mode,
 				code,
-				async () => {
-					await this.initializeAsync(async () => {
-						await Promise.all([
-							TrackingUtility.trackAsync(await this.configSvc.getResourceAsync("common.loading.activate"), `users/activate/${mode}`),
-							this.showActivationResultAsync({
-								Status: "OK",
-								Mode: mode
-							})
-						]);
-					}, true);
-				},
-				async error => {
-					await this.initializeAsync(async () => {
-						await this.showActivationResultAsync({
-							Status: "Error",
-							Mode: mode,
-							Error: error
-						});
-					});
-				}
+				() => this.initializeAsync(async () => {
+					await Promise.all([
+						TrackingUtility.trackAsync(await this.configSvc.getResourceAsync("common.loading.activate"), `users/activate/${mode}`),
+						this.showActivationResultAsync({
+							Status: "OK",
+							Mode: mode
+						})
+					]);
+				}, true),
+				error => this.initializeAsync(async () => await this.showActivationResultAsync({
+					Status: "Error",
+					Mode: mode,
+					Error: error
+				}))
 			);
 		}
 		else {
-			await this.initializeAsync(async () => {
-				await this.showActivationResultAsync({
-					Status: "Error",
-					Mode: mode,
-					Error: {
-						Message: await this.configSvc.getResourceAsync("users.activate.messages.error.invalid", { mode: mode, code: code })
-					}
-				});
-			});
+			await this.initializeAsync(async () => await this.showActivationResultAsync({
+				Status: "Error",
+				Mode: mode,
+				Error: {
+					Message: await this.configSvc.getResourceAsync("users.activate.messages.error.invalid", { mode: mode, code: code })
+				}
+			}));
 		}
 	}
 
